17cdaec0ad934f3b7aa52c8777f6aa16c9d3a86a
thiagoaai <dockplus@dockplusai.com>
2026-04-24 22:09:03 -0400

    switch production domain to thiagao.io

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const siteUrl = 'https://thiagaoai.io';
+const siteUrl = 'https://thiagao.io';
 
 export default function robots(): MetadataRoute.Robots {
   return {
